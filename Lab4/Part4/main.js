3c4e3618f07272b5795239c03bc0e0a5a3f63674
NatdaCtkm <natda.chanthakoumman@dcmail.ca>
2026-07-31 22:22:47 -0400

    Remove some commented  main.js

--- Lab4/Part4/main.js
+++ Lab4/Part4/main.js
@@ -12,17 +12,14 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random RGB color
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-// Shape parent class
 class Shape {
   constructor(x, y, velX, velY) {
     this.x = x;
@@ -32,7 +29,6 @@ class Shape {
   }
 }
 
-// Ball class extends Shape
 class Ball extends Shape {
   constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY);
@@ -79,7 +75,6 @@ class Ball extends Shape {
   }
 }
 
-// EvilCircle class extends Shape
 class EvilCircle extends Shape {
   constructor(x, y) {
     super(x, y, 20, 20);
@@ -124,7 +119,6 @@ class EvilCircle extends Shape {
   }
 }
 
-// Create 25 balls
 const balls = [];
 
 while (balls.length < 25) {
@@ -140,10 +134,8 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
-// Create evil circle
 const evil = new EvilCircle(random(0, width), random(0, height));
 
-// Move evil circle with mouse
 window.addEventListener("mousemove", (e) => {
   evil.x = e.x;
   evil.y = e.y;
